99d5a93f1882d692c4791f70d05bba0e713f4bcf
Igor Becker <igor_nyu@hotmail.com>
2024-04-16 15:32:13 -0300

    feat: new component for products

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -49,18 +49,19 @@ import * as $$$$$$34 from "./sections/Miscellaneous/CampaignTimer.tsx";
 import * as $$$$$$35 from "./sections/Miscellaneous/CookieConsent.tsx";
 import * as $$$$$$36 from "./sections/Miscellaneous/Slide.tsx";
 import * as $$$$$$37 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$38 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$39 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$40 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$41 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$42 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$43 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$44 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$45 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$46 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$47 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$48 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$49 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$38 from "./sections/Product/HorizontalProductCard.tsx";
+import * as $$$$$$39 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$40 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$41 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$42 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$43 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$44 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$45 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$46 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$47 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$48 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$49 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$50 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -113,19 +114,21 @@ const manifest = {
       $$$$$$35,
     "deco-sites/igortemplate/sections/Miscellaneous/Slide.tsx": $$$$$$36,
     "deco-sites/igortemplate/sections/Newsletter/Newsletter.tsx": $$$$$$37,
-    "deco-sites/igortemplate/sections/Product/ImageGalleryFrontBack.tsx":
+    "deco-sites/igortemplate/sections/Product/HorizontalProductCard.tsx":
       $$$$$$38,
-    "deco-sites/igortemplate/sections/Product/NotFound.tsx": $$$$$$39,
-    "deco-sites/igortemplate/sections/Product/NotFoundChallenge.tsx": $$$$$$40,
-    "deco-sites/igortemplate/sections/Product/ProductDetails.tsx": $$$$$$41,
-    "deco-sites/igortemplate/sections/Product/ProductShelf.tsx": $$$$$$42,
-    "deco-sites/igortemplate/sections/Product/ProductShelfTabbed.tsx": $$$$$$43,
-    "deco-sites/igortemplate/sections/Product/SearchResult.tsx": $$$$$$44,
-    "deco-sites/igortemplate/sections/Product/ShelfWithImage.tsx": $$$$$$45,
-    "deco-sites/igortemplate/sections/Product/Wishlist.tsx": $$$$$$46,
-    "deco-sites/igortemplate/sections/Social/InstagramPosts.tsx": $$$$$$47,
-    "deco-sites/igortemplate/sections/Social/WhatsApp.tsx": $$$$$$48,
-    "deco-sites/igortemplate/sections/Theme/Theme.tsx": $$$$$$49,
+    "deco-sites/igortemplate/sections/Product/ImageGalleryFrontBack.tsx":
+      $$$$$$39,
+    "deco-sites/igortemplate/sections/Product/NotFound.tsx": $$$$$$40,
+    "deco-sites/igortemplate/sections/Product/NotFoundChallenge.tsx": $$$$$$41,
+    "deco-sites/igortemplate/sections/Product/ProductDetails.tsx": $$$$$$42,
+    "deco-sites/igortemplate/sections/Product/ProductShelf.tsx": $$$$$$43,
+    "deco-sites/igortemplate/sections/Product/ProductShelfTabbed.tsx": $$$$$$44,
+    "deco-sites/igortemplate/sections/Product/SearchResult.tsx": $$$$$$45,
+    "deco-sites/igortemplate/sections/Product/ShelfWithImage.tsx": $$$$$$46,
+    "deco-sites/igortemplate/sections/Product/Wishlist.tsx": $$$$$$47,
+    "deco-sites/igortemplate/sections/Social/InstagramPosts.tsx": $$$$$$48,
+    "deco-sites/igortemplate/sections/Social/WhatsApp.tsx": $$$$$$49,
+    "deco-sites/igortemplate/sections/Theme/Theme.tsx": $$$$$$50,
   },
   "matchers": {
     "deco-sites/igortemplate/matchers/platform.ts": $$$$$$$0,
--- sections/Product/HorizontalProductCard.tsx
+++ sections/Product/HorizontalProductCard.tsx
@@ -1,22 +1,75 @@
-import {ProductDetailsPage} from 'apps/commerce/types.ts';
-import ProductDetails from 'deco-sites/igortemplate/sections/Product/ProductDetails.tsx';
-import ImageGallerySlider from 'deco-sites/igortemplate/components/product/Gallery/ImageSlider.tsx';
-import ProductInfo from 'deco-sites/igortemplate/components/product/ProductInfo.tsx';
+import { ProductDetailsPage } from "apps/commerce/types.ts";
 
 export interface Props {
-  page: ProductDetailsPage;
+  product: ProductDetailsPage | null;
+}
+
+function formatCurrency(price: number | undefined) {
+  if (!price) {
+    return undefined;
+  }
+  return Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
+}
+
+function getProductProps(productDetails: ProductDetailsPage) {
+  const { product } = productDetails;
+  return {
+    image: product?.image,
+    url: product?.url,
+    description: product?.description,
+    name: product?.name,
+    offers: product?.offers,
+  };
 }
 
 export default function HorizontalProductCard(props: Props) {
-  return (
-    <div className="w-full">
-      <div className="lg:hidden md:flex flex-col">
-        <ImageGallerySlider page={props.page} />
-        <ProductInfo page={props.page} />
-      </div>
+  if (!props?.product) {
+    return null;
+  }
 
-      <div className="lg:flex md:hidden items-start justify-center">
-        <ProductDetails page={props.page} />
+  const { image, url, description, name, offers } = getProductProps(
+    props.product,
+  );
+
+  const priceIsDiferent = offers?.lowPrice !== offers?.highPrice;
+
+  return (
+    <div className="w-full h-auto flex items-center justify-center my-2">
+      <div className="flex flex-col lg:flex-row items-center lg:items-start justify-center border-black rounded border px-2 py-2 bg-gray-800 gap-4">
+        <img
+          src={image?.[0].url}
+          height={100}
+          width={100}
+          className="hidden lg:block"
+        />
+        <img
+          src={image?.[0].url}
+          height={200}
+          width={200}
+          className="block lg:hidden"
+        />
+
+        <div className="flex flex-col items-start text-white">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+
+        <div class="flex flex-col items-start justify-between text-white">
+          {priceIsDiferent && <span>{formatCurrency(offers?.lowPrice)}</span>}
+          <span className={`${priceIsDiferent ? "text-red-50" : ""}`}>
+            <s>{formatCurrency(offers?.highPrice)}</s>
+          </span>
+
+          <a
+            href={url}
+            className="text-white px-2 py-2 bg-red-500 hover:bg-red-700 transition:ease-in-out"
+          >
+            Details
+          </a>
+        </div>
       </div>
     </div>
   );
